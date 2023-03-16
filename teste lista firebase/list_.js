57a1b745f9eb0db3509a8177ee8d93d97685da6f
José Augusto Nascimento Rosa Santos <101712645+joseaugustorosa@users.noreply.github.com>
2023-03-16 17:05:32 -0300

    Add files via upload

---
+++ teste lista firebase/list_.js
@@ -0,0 +1,74 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCsRE9UQOLS8OtuvEUjL7_SlsmZPV-d-Bg",
+    authDomain: "jogatina-2a7a2.firebaseapp.com",
+    projectId: "jogatina-2a7a2",
+    storageBucket: "jogatina-2a7a2.appspot.com",
+    messagingSenderId: "866150906247",
+    appId: "1:866150906247:web:7649251def3a0f5417bb22"
+  };
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+
+
+
+const userRef = firebase.firestore().collection('numero').doc('305cuhPD12qNCZr86h12');
+const petsRef = userRef.collection('informações');
+
+// obtém um QuerySnapshot da subcoleção "pets"
+petsRef.get()
+  .then((querySnapshot) => {
+    const info = querySnapshot.forEach((doc) => {
+    doc.data()
+      
+    });
+    addTask(info);
+  })
+  
+
+
+
+
+function addTask(info){
+    const main = document.getElementById('bloco_tarefas_pessoais');
+    const prod = JSON.parse(localStorage.getItem('product'));
+
+    info.forEach( info =>{
+
+        const container_item = document.createElement('div');
+        container_item.classList.add('container_tp');
+        main.appendChild(container_item);
+        container_item.id = info.uid;
+        
+
+
+        const h2 = document.createElement('h2');
+        h2.classList.add('title_tp');
+        container_item.appendChild(h2);
+        h2.innerHTML = info.nome;
+        const h = document.createElement('h2');
+        h.classList.add('title_tp');
+        container_item.appendChild(h2);
+        h.innerHTML = prod.nome;
+
+
+       
+
+      
+        const div_btn =document.createElement('div');
+        div_btn.classList.add('btn_container');
+        container_item.appendChild(div_btn);
+
+        const button = document.createElement('button');
+        button.classList.add('feito_tp');
+        div_btn.appendChild(button);
+        button.addEventListener('click', event =>{
+            event.stopPropagation();
+            IR(task);
+           
+        })
+        button.innerHTML = "feito";
+
+    })
+}
